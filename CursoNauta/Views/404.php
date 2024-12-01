69bc6848ef2c54edff0942d9561a2cb2eba0b5bc
romordz <99628261+romordz@users.noreply.github.com>
2024-12-01 15:17:39 -0600

    Delete CursoNauta directory

--- CursoNauta/Views/404.php
+++
@@ -1,4 +0,0 @@
-<link rel="stylesheet" href="Views/css/SPrincipal.css">
-
-<h1>404 - Página no encontrada</h1>
-<p>Lo sentimos, pero la página que buscas no existe.</p>
--- CursoNauta/Views/Parciales/Footer.php
+++
@@ -1,8 +0,0 @@
-<footer id="contacto">
-        <p>© 2024 CursoNauta. Todos los derechos reservados.</p>
-        <p>Contacto: info@cursonauta.com</p>
-</footer>
-
-</body>
-
-</html>
