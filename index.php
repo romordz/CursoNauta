6ce1f6ce792ed22e52f9042745b0ec58911fed0d
romordz <rickyromo32@hotmail.com>
2026-08-22 22:42:06 -0600

    Refactor index.php to streamline dotenv loading and remove redundant code

--- index.php
+++ index.php
@@ -1,15 +1,13 @@
 <?php
+require_once 'vendor/autoload.php';
+if (file_exists(__DIR__ . '/.env')) {
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->safeLoad(); 
+}
 include_once 'Models/Database.php';
 include 'Models/Router.php';
-require_once 'vendor/autoload.php';
 require_once 'init.php';
 $database = new Database();
 $db = $database->getConnection();
 $router = new Router('routes.php');
 $router->handleRequest();
-
-
-if (file_exists(__DIR__ . '/.env')) {
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-    $dotenv->load();
-}
